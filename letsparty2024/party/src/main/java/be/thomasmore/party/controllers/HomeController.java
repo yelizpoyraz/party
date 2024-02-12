e67500fb213741b7a00fb6b2f9643a8e27fc3e63
yeliz <yelizpyrz09@gmail.com>
2024-02-12 12:23:17 +0100

    Dynamic page exercise

--- letsparty2024/party/src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ letsparty2024/party/src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -1,2 +1,28 @@
-package be.thomasmore.party.controllers;public class HomeController {
+package be.thomasmore.party.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String home(Model model){
+        int myCalculatedValue = 34 * 62;
+        model.addAttribute("myCalculatedValue", myCalculatedValue);
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model){
+        String name= "Yeliz";
+        String address = "Brusselstraat 67";
+        String city = "Antwerpen";
+        model.addAttribute("name", name);
+        model.addAttribute("address", address);
+        model.addAttribute("city", city);
+        return  "about";
+    }
 }
